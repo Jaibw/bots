b7b84023f8b3c35672f336cdfd1b48854be7361c
bhagwan.jai <bhagwan.jai@infoedge.com>
2024-09-11 12:54:42 +0530

    shorten output

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("Welcome to the AI Bot")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
