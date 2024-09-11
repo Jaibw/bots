5b745ae0b40c9c7f4448aae0851a3df2a773930e
bhagwan.jai <bhagwan.jai@infoedge.com>
2024-09-11 13:19:58 +0530

    added ldap libs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,35 @@
 import streamlit as st
+from streamlit_rsa_auth_ui import SignoutEvent
+from streamlit_ldap_authenticator import Authenticate, UserInfos
 
-st.title("Welcome to the AI Bot")
-# st.write(
-#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-# )
+from typing import Optional, Union, Literal
+
+# Declare the authentication object
+auth = Authenticate(
+    st.secrets['ldap'],
+    st.secrets['session_state_names'],
+    st.secrets['auth_cookie']
+)
+
+
+def login(user: Union[UserInfos, str]) -> Optional[str]:
+    st.session_state.TestSs = {"login_successful": True}
+
+
+def logout(event: SignoutEvent) -> Optional[Literal['cancel']]:
+    if 'TestSs' in st.session_state:
+        del st.session_state.TestSs
+    if 'TestSs' in st.session_state:
+        return 'cancel'
+
+
+# Login Process
+user = auth.login(callback=login)
+if user is not None:
+    # st.set_page_config(initial_sidebar_state='expanded',)
+    auth.createLogoutForm({'message': f"Welcome {user['displayName']}"}, callback=logout)
+
+    # Your page application can be written below
+    st.write("# Welcome to my App! 👋")
+    st.write(user)
+
